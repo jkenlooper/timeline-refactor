fb2841f44332a71bd076eb0243d41908aa941f34
Jake Hickenlooper <jake@weboftomorrow.com>
2018-10-30 06:27:56 -0600

    Move fetching of events to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { EventsService } from './events.service';
+import { Event } from './event';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'timeline';
+  events: Event[];
+
+  constructor(private eventsService: EventsService) { }
+
+  ngOnInit() {
+    this.getEvents();
+  }
+
+  getEvents(): void {
+    this.eventsService.getEvents()
+    .subscribe((events) => {
+      console.log(events);
+      this.events = events;
+    });
+  }
+
+  setEvents(events): void {
+    this.events = events;
+  }
+
 }
--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,40 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Event } from '../event';
-import { EventsService } from '../events.service';
 
 @Component({
   selector: 'app-event-list',
   template: `
-      <div>
-        <app-new-event (reloadRequest)="setEvents($event)"></app-new-event>
-        <ol>
-          <li *ngFor="let event of events">
-            <app-timeline-event [event]="event"></app-timeline-event>
-          </li>
-        </ol>
-      </div>
+    <ol>
+      <li *ngFor="let event of filteredEvents">
+        <app-timeline-event [event]="event"></app-timeline-event>
+      </li>
+    </ol>
   `,
   styleUrls: ['./event-list.component.css']
 })
-export class EventListComponent implements OnInit {
-  events: Event[];
+export class EventListComponent {
 
-  constructor(private eventsService: EventsService) { }
+  filteredEvents: Event[];
 
-  ngOnInit() {
-    this.getEvents();
+  @Input()
+  set events (list) {
+    this.filteredEvents = list.reverse();
   }
 
-  setEvents(events): void {
-    console.log('setEvents', events);
-    this.events = events.reverse();
-  }
-
-  getEvents(): void {
-    this.eventsService.getEvents()
-    .subscribe((events) => {
-      this.setEvents(events);
-    });
-  }
-
+  constructor() { }
 }
